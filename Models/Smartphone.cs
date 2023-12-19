0b68c0e38927bfd6c8aa60ca208144b3e981f28a
Regis <regis_ferneda@hotmail.com>
2023-12-19 11:33:12 -0300

    projeto poo final

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,13 +4,19 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
+            // TODO: Passar os parâmetros do construtor para as propriedades 
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;                           
         }
-
+       
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia(numero: "(11)998575441", modelo: "Modelo 1", imei: "88887777", memoria: 64);
+Console.WriteLine($"Smartphone Nokia: {nokia.Numero} ");
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+
+Smartphone iphone = new Iphone(numero: "(11)965545622", modelo: "Modelo 2", imei: "22225555", memoria: 128);
+Console.WriteLine($"Smartphone iPhone: {iphone.Numero}");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
